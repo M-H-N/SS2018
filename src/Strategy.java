d162a8980437425b7fc1e45263f6cb720bfd1421
ASUS <mhn.work@yahoo.com>
2018-12-02 01:33:58 +0330

    All Classes are in the root src directory(package)[as rules of the game]. Some Uncommented outputs are now commented. Indirect defence added again to the 'canTakeTheBallAway' Method. Issues with 'OwnGoal' Class solved. Issues with 'SuperDefence' Class solved. Time Checking output is removed. WHAT-NEEDS-TO-BE-DONE:  I-if none of the defencing strategies worked, find the player of enemy who can goal directly and hit it!  II-the 'canTakeTheBallAway' method needs some improvements.

--- src/Strategy.java
+++ src/Strategy.java
@@ -1,7 +1,3 @@
-
-
-import java.util.Date;
-
 /**
  * @author mdsinalpha
  */
@@ -73,11 +69,9 @@ public class Strategy {
 //        act.setAngle(new Random().nextInt() % 360);
 //        act.setPower(100);
 
-        System.out.println(new Date().getTime());
         MHN_AI ai = new MHN_AI(act, game);
         ai.action();
         System.out.println("COMMANDED--> ANGLE: " + act.getAngle() + " , POWER: " + act.getPower() + " , PLAYER: " + act.getPlayer_id());
-        System.out.println(new Date().getTime());
         return act;
     }
 }
